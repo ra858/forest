1258b3275f31ab23435aedd930cc4648499c1c6c
ra858 <deathbag1@proton.me>
2026-06-22 10:07:14 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+rm -rf build/ output/
+forester build forest.toml
+
+cd ../forest-deploy
+git rm -rf . >/dev/null 2>&1 || true
+git clean -fxd
+
+cd ../forest
+cp -a ./output/forest/. ../forest-deploy
+
+git add .
+git commit -m "deploy"
+git push
